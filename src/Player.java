fd5b4c10072f339c36b66bbf261b66a3699c4945
jackmcdonnell <jack_mcdonnell28@milton.edu>
2026-05-09 19:37:48 -0400

    gave the player a bunch more blackjack related atributes (whether or not they busted, having a blackjack, etc...) and I made a method that resets the game after a round. I learned how to use the "return" tool better than I previously did (after thorough research it is much simpler than I thought and just sends a value back to wherever the method was called).

--- src/Player.java
+++ src/Player.java
@@ -1,40 +1,85 @@
 public class Player {
-    public int value;
+
     public int numPlayer;
-    public String suit;
     public boolean isFaceUp;
-    public boolean hasGamlingProblem;
+    public boolean hasGamblingProblem;
     public boolean isDealer = false;
+
     public Card[] hand;
     public int numCards;
+
+
     public static void main(String[] args) {
-Player Craig = new Player(1);
+        Player Craig = new Player(1);
     }
-    public Player(int pnumPlayer){
+
+    public Player(int pnumPlayer) {
         numPlayer = pnumPlayer;
-hasGamlingProblem = true;
-numCards = 0;
-hand = new Card[11];
-isFaceUp = true;
-printPlayer();
+
+        hasGamblingProblem = true; //this boolean doesnt do anything I just thought it was funny
+        numCards = 0;
+
+        hand = new Card[11]; // this is the max amount of cards in blackjack
+        isFaceUp = true;
+
+
     }
-    public void printPlayer(){
-        for(int i = 0; i < numCards; i ++) {
+
+    // this one prints all the cards in hand
+    public void printPlayer() {
+
+        System.out.println("Player " + numPlayer + "'s Hand:");
+
+        for (int i = 0; i < numCards; i++) {
             hand[i].printInfo();
         }
-    }
-    public void addCard(Card newCard){
-        hand[numCards] = newCard;
-        numCards++;
 
-    }
-
-    public void sumCards(){
-         int sumhand = 0;
-//the combined value of every card in ur hand
-        for (int i = 0; i < numCards; i ++){
-           sumhand =  hand[i].value + sumhand;
-        }
+        System.out.println("Hand Value: " + sumCards());
         System.out.println();
+    }
+
+    // this adds a card to the player's hand
+    public void addCard(Card newCard) {
+
+        if (numCards < hand.length) {
+            hand[numCards] = newCard;
+            numCards++;
+        }
+    }
+
+    // this calculates the value of the hand
+    public int sumCards() {
+
+        int sumHand = 0;
+        int numAces = 0;
+
+        for (int i = 0; i < numCards; i++) {
+
+            int value = hand[i].value;
+
+            // this helps with managing aces and their values
+
+        }
+
+        //  over 21, turn aces into 1 instead of 11
+
+        return sumHand;
+    }
+
+    // Checks if player busted
+    public boolean isBusted() {
+        return sumCards() > 21;
+    }
+
+    // Checks for blackjack
+    public boolean hasBlackjack() {
+        return sumCards() == 21 && numCards == 2;
+    }
+
+    // Clears hand for next round
+    public void resetHand() {
+
+        hand = new Card[11];
+        numCards = 0;
     }
 }
